c7a3fd23a54117b29bd4e5f425f98027d28e4ffd
Fi2a <fia_to@naver.com>
2025-06-27 12:39:48 +0900

    재원 0627

--- src/main/java/com/salon/constant/ServiceCategory.java
+++ src/main/java/com/salon/constant/ServiceCategory.java
@@ -1,7 +1,7 @@
 package com.salon.constant;
 
 public enum ServiceCategory {
-    CUT("커트"), COLOR("염색"), PREM("펌"), UPSTYLE("업스타일"), DRY("드라이"), HAIR_EXTENSION("붙임머리"), CLINIC("클리닉");
+    CUT("커트"), COLOR("염색"), PERM("펌"), UPSTYLE("업스타일"), DRY("드라이"), HAIR_EXTENSION("붙임머리"), CLINIC("클리닉");
 
     private final String label;
 
--- src/main/java/com/salon/repository/management/master/ServiceRepo.java
+++ src/main/java/com/salon/repository/management/master/ServiceRepo.java
@@ -1,5 +1,6 @@
 package com.salon.repository.management.master;
 
+import com.salon.constant.ServiceCategory;
 import com.salon.entity.management.master.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,8 @@ public interface ServiceRepo extends JpaRepository<Service, Long> {
 
     // 미용실 시술 목록
     List<Service> findByShopId(Long shopId);
+
+    // 미용사 담당 시술목록 (카테고리별로)
+    List<Service> findByDesignerIdAndCategoryIn(Long id, List<ServiceCategory> categories);
+
 }
