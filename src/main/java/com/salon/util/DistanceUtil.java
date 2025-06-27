2c2f314bf40945f64213bb7c094bd41b08966acf
dmsgml7476 <dmsgml7476@gmail.com>
2025-06-27 20:49:46 +0900

    0627 풀리퀘 dto 작업중2

---
+++ src/main/java/com/salon/util/DistanceUtil.java
@@ -0,0 +1,11 @@
+package com.salon.util;
+
+import java.math.BigDecimal;
+
+public class DistanceUtil {
+
+    public static BigDecimal caculateDistance(BigDecimal lat1, BigDecimal lon1, BigDecimal lat2, BigDecimal lon2) {
+
+    }
+
+}
