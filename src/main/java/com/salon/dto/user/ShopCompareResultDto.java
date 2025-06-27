d4dae5ac611ecfee9927f24fd3e5fa9de4171f53
dmsgml7476 <dmsgml7476@gmail.com>
2025-06-27 20:05:23 +0900

    0627 풀리퀘 dto 작업중2

--- src/main/java/com/salon/dto/user/ShopCompareResultDto.java
+++ src/main/java/com/salon/dto/user/ShopCompareResultDto.java
@@ -1,14 +1,28 @@
 package com.salon.dto.user;
 
+import com.salon.dto.management.ServiceForm;
 import com.salon.dto.shop.ShopListDto;
+import com.salon.entity.shop.Shop;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
 public class ShopCompareResultDto {
     private Long Id; // 선택된 미용실 아이디
     private ShopListDto shopListDto;
+    private List<ServiceForm> serviceForms;
+
+    public static ShopCompareResultDto from(Shop shop, ShopListDto shopListDto, List<ServiceForm> serviceForms) {
+        ShopCompareResultDto dto = new ShopCompareResultDto();
+
+        dto.setId(shop.getId());
+        dto.setShopListDto(shopListDto);
+        dto.setServiceForms(serviceForms);
+
+        return dto;
+    }
 }
