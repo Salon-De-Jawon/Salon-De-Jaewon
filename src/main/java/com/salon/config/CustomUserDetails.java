6fac6a0a3d71406b80e0e22e48493b191baad673
Fi2a <fia_to@naver.com>
2025-06-27 12:57:21 +0900

    재원 0627

---
+++ src/main/java/com/salon/config/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.salon.config;
+
+import com.salon.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + member.getRole())); // 예: ROLE_USER, ROLE_ADMIN
+    }
+
+    public Member getUser(){
+        return this.member;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword(); // 암호화된 비밀번호
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getLoginId(); // 로그인 ID
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+}
