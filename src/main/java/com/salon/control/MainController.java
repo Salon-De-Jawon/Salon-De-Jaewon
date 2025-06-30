608a491f3df9f280668f60855af99b0e0d2a1e2c
dmsgml7476 <dmsgml7476@gmail.com>
2025-06-30 16:31:51 +0900

    헤더/시큐리티 수정

--- src/main/java/com/salon/control/MainController.java
+++ src/main/java/com/salon/control/MainController.java
@@ -1,6 +1,9 @@
 package com.salon.control;
 
+import com.salon.config.CustomUserDetails;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +19,7 @@ public class MainController {
         return "/mainpage";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/loginpage")
     public  String loginPage() {
         return "/user/login";
     }
