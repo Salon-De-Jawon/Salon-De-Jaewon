b3aac9769e142e468d960eaceef2d381db2c5226
dmsgml7476 <dmsgml7476@gmail.com>
2025-07-03 23:29:16 +0900

    DistanceUil 추가

--- src/main/resources/static/javascript/user/signUp.js
+++ src/main/resources/static/javascript/user/signUp.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if(!loginId) {
             idMsg.textContent = "아이디를 입력해주세요";
-            idMsg.style.color = "red";
+            idMsg.style.color = "var(--red-msg)";
             idMsg.style.display = "block";
             isIdValid=false;
             return;
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if(loginId.length < 6 || loginId.length > 16) {
             idMsg.textContent = "아이디는 6자 이상 16자 이하로 입력해주세요."
-            idMsg.style.color = "red";
+            idMsg.style.color = "var(--red-msg)";
             idMsg.style.display = "block";
             isIdValid=false;
             return;
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if(!regex.test(loginId)) {
             idMsg.textContent = "아이디는 영어 소문자, 숫자, 언더바(_)만 사용할 수 있습니다.";
-            idMsg.style.color = "red";
+            idMsg.style.color = "var(--red-msg)";
             idMsg.style.display = "block";
             isIdValid=false;
             return;
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 if (data.exists) {
                     idMsg.textContent = "이미 사용 중인 아이디입니다.";
-                    idMsg.style.color = "red"
+                    idMsg.style.color = "var(--red-msg)";
                     idMsg.style.display = "block";
                     isIdValid=false;
                 } else {
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
             })
             .catch(() => {
                 idMsg.textContent = "서버 오류가 발생했습니다.";
-                idMsg.style.color = "red";
+                idMsg.style.color = "var(--red-msg)";
                 idMsg.style.display = "block";
                 isIdValid=false;
             });
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if(!allowedCharsOnly) {
             pwMsg.textContent="특수문자는 !,@,#,$,%,^,&,* 만 사용할 수 있습니다.";
             pwMsg.style.display="block";
-            pwMsg.style.color = "red";
+            pwMsg.style.color = "var(--red-msg)";
             isPwValid = false;
             checkPwMatch();
             return;
@@ -105,14 +105,14 @@ document.addEventListener("DOMContentLoaded", function () {
         if(!lengthValid) {
             pwMsg.textContent="비밀번호는 8자 이상 30자 이하로 입력하세요.";
             pwMsg.style.display="block";
-            pwMsg.style.color = "red";
+            pwMsg.style.color = "var(--red-msg)";
             isPwValid = false;
             checkPwMatch();
             return;
         } else if (!(containsLetter && containsDigit)) {
             pwMsg.textContent="영문자와 숫자가 최소 하나씩은 포함되어있어야 합니다.";
             pwMsg.style.display="block";
-            pwMsg.style.color = "red";
+            pwMsg.style.color = "var(--red-msg)";
             isPwValid = false;
             checkPwMatch();
            return;
@@ -146,7 +146,7 @@ document.addEventListener("DOMContentLoaded", function () {
         isPwMatch = true;
       } else if (pwChk.length > 0) {
         pwMismatchMsg.textContent = "비밀번호가 일치하지 않습니다.";
-        pwMismatchMsg.style.color = "red";
+        pwMismatchMsg.style.color = "var(--red-msg)";
         pwMismatchMsg.style.display = "block";
         isPwMatch = false;
       } else {
@@ -154,8 +154,6 @@ document.addEventListener("DOMContentLoaded", function () {
         pwMismatchMsg.style.display = "none";
         isPwMatch = false;
       }
-
-      checkValidState();
     }
 
 
@@ -175,6 +173,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const csrfToken = document.getElementById("csrfToken").value;
     const csrfHeader = document.getElementById("csrfHeader").value;
 
+    let isEmailValid = false;
+    let isEmailVerified = false;
+
+    emailInput.addEventListener("input", () => {
+        isEmailValid = false;
+        isEmailVerified = false;
+        emailMsg.textContent = "";
+        codeMsg.textContent = "";
+        codeMsg.style.display = "none";
+        verifyBtn.disabled = false;
+    });
+
     sendBtn.addEventListener("click", async function(e) {
         e.preventDefault();
 
@@ -182,15 +192,30 @@ document.addEventListener("DOMContentLoaded", function () {
         emailMsg.style.display = "none";
 
 
+
+
+
         if(!email) {
            emailMsg.textContent = "이메일을 입력하세요";
+           emailMsg.style.color = "var(--red-msg)";
            emailMsg.style.display = "block";
+           isEmailValid = false;
+           return;
+        }
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailPattern.test(email)) {
+           emailMsg.textContent = "유효한 이메일 주소를 입력해주세요.";
+           emailMsg.style.color = "var(--red-msg)";
+           emailMsg.style.display = "block";
+           isEmailValid = false;
            return;
         }
 
         try {
            const res = await fetch("/auth/email/check", {
-           method: "post",
+           method: "POST",
            headers: {"Content-Type": "application/json",
                [csrfHeader]: csrfToken
            },
@@ -201,18 +226,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
            if(result.exists) {
               emailMsg.textContent = "이미 존재하는 이메일입니다.";
-              emailMsg.style.color = "red";
+              emailMsg.style.color = "var(--red-msg)";
               emailMsg.style.display = "block";
+              isEmailValid = false;
               return;
            }
 
+           isEmailValid = true;
            emailMsg.textContent = "인증번호가 전송되었습니다.";
            emailMsg.style.color = "green";
            emailMsg.style.display = "block";
            codeInput.focus();
 
-
-           verifyBtn.disabled = false;
 
            fetch("/auth/email/send", {
              method: "POST",
@@ -226,17 +251,18 @@ document.addEventListener("DOMContentLoaded", function () {
               .then(data => {
                  if(data.status !== "sent") {
                    emailMsg.textContent = "이메일 전송에 실패했습니다";
-                   emailMsg.style.color = "red";
+                   emailMsg.style.color = "var(--red-msg)";
+                   isEmailValid = false;
                  }
               })
               .catch(() => {
                  emailMsg.textContent = "서버 오류가 발생했습니다.";
-                 emailMsg.style.color = "red";
+                 emailMsg.style.color = "var(--red-msg)";
               });
 
         } catch (err) {
             emailMsg.textContent = "서버 오류가 발생했습니다.";
-            emailMsg.style.color = "red";
+            emailMsg.style.color = "var(--red-msg)";
             emailMsg.style.display = "block";
         }
     });
@@ -249,7 +275,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
        if(!code) {
           codeMsg.textContent = "인증번호를 입력해주세요.";
-          codeMsg.style.color = "red";
+          codeMsg.style.color = "var(--red-msg)";
           codeMsg.style.display = "block";
           return;
        }
@@ -265,18 +291,21 @@ document.addEventListener("DOMContentLoaded", function () {
               if(data.success) {
                  codeMsg.textContent="인증되었습니다.";
                  codeMsg.style.color = "green";
-
+                 codeMsg.style.display = "block";
                  verifyBtn.disabled = true;
+                 isEmailVerified = true;
               } else {
                  codeMsg.textContent = "인증번호가 일치하지 않습니다";
-                 codeMsg.style.color = "red";
+                 codeMsg.style.color = "var(--red-msg)";
+                 codeMsg.style.display = "block";
+                 isEmailVerified = false;
 
               }
               codeMsg.style.display = "block";
            })
            .catch(() => {
               codeMsg.textContent = "서버 오류가 발생했습니다.";
-              codeMsg.style.color = "red";
+              codeMsg.style.color = "var(--red-msg)";
               codeMsg.style.display = "block";
 
            });
@@ -284,11 +313,121 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 이메일 인증 끝
 
+    const nameInput = document.getElementById("name");
+    const birthDateInput = document.getElementById("birthDate");
+    const genderInputs = document.querySelectorAll("input[name='gender']");
+    const telInput = document.getElementById("tel");
+
+    const nameErrorMsg = document.getElementById("nameErrorMsg");
+    const formGroupMsg = document.getElementById("formGroupMsg");
+    const telErrorMsg = document.getElementById("telErrorMsg");
+
+
+
+    function validateForm() {
+        const loginId = loginIdInput.value.trim();
+        const regex = /^[a-z0-9_]+$/;
+
+        if (!loginId || loginId.length < 6 || loginId.length > 16 || !regex.test(loginId)) {
+            idMsg.textContent = "아이디를 형식에 맞게 입력해주세요."
+            idMsg.style.color = "red";
+            idMsg.style.display = "block";
+            loginIdInput.focus();
+            return false;
+        }
+
+        if (!isIdValid) {
+            idMsg.textContent = "아이디 중북 여부를 확인해주세요."
+            idMsg.style.color = "var(--red-msg)";
+            idMsg.style.display = "block";
+            chkLoginIdBtn.focus();
+            return false;
+        }
+
+        if(!isPwValid) {
+            pwMsg.textContent="비밀번호를 확인해주세요.";
+            pwMsg.style.display="block";
+            pwMsg.style.color = "var(--red-msg)";
+            pwInput.focus();
+            return false;
+        }
+
+        if(!isPwMatch) {
+            pwMismatchMsg.textContent = "비밀번호가 일치하지 않습니다.";
+            pwMismatchMsg.style.color = "var(--red-msg)";
+            pwMismatchMsg.style.display = "block";
+            pwChkInput.focus();
+            return false;
+        }
+
+        if (!isEmailValid) {
+           emailMsg.textContent = "이메일 중복을 확인해주세요";
+           emailMsg.style.color = "var(--red-msg)";
+           emailMsg.style.display = "block";
+           emailInput.focus();
+           return false;
+        }
+
+        if (!isEmailVerified) {
+          codeMsg.textContent = "인증번호를 확인해주세요.";
+          codeMsg.style.color = "var(--red-msg)";
+          codeMsg.style.display = "block";
+          codeInput.focus();
+          return false;
+        }
+
+        const nameValue = nameInput.value.trim();
+
+        if (!nameValue) {
+          nameErrorMsg.textContent = "본명을 입력해주세요.";
+          nameErrorMsg.style.color = "var(--red-msg)";
+          nameErrorMsg.style.display = "block";
+          nameInput.focus();
+          return false;
+        }
+
+        if (!birthDateInput.value) {
+          formGroupMsg.textContent = "생년월일을 입력해주세요.";
+          formGroupMsg.style.color = "var(--red-msg)";
+          formGroupMsg.style.display = "block";
+          birthDateInput.focus();
+          return false;
+        }
+
+        const selectedGender = Array.from(genderInputs).some(input => input.checked);
+        if (!selectedGender) {
+          formGroupMsg.textContent = "성별을 입력해주세요.";
+          formGroupMsg.style.color = "var(--red-msg)";
+          formGroupMsg.style.display = "block";
+          genderInputs[0].focus();
+          return false;
+        }
+
+        const telValue = telInput.value.trim();
+
+        if (!telValue) {
+          telErrorMsg.textContent = "전화번호를 입력하세요.";
+          telErrorMsg.style.color = "var(--red-msg)";
+          telErrorMsg.style.display = "block";
+          nameInput.focus();
+          return false;
+        }
+
+
+        return true;
+
+    }
+
 
     const form = document.querySelector("form");
 
     form.addEventListener("submit", function (e) {
         e.preventDefault();
+
+        if (validateForm()) {
+            setTimeout(() => form.submit(), 100);
+        }
+
     });
 
 
