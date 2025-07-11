8d4879f43b37a2c32980416781a0fcedce4647f3
eunjin466 <dmswls10917@naver.com>
2025-07-11 17:32:11 +0900

    ㅇ왕왕왕왕와아ㅏ아앙...

--- src/main/java/com/salon/control/ReservationController.java
+++ src/main/java/com/salon/control/ReservationController.java
@@ -3,17 +3,22 @@ package com.salon.control;
 
 import com.salon.dto.designer.DesignerListDto;
 import com.salon.dto.management.MemberCouponDto;
-import com.salon.dto.shop.DesignerServiceCategoryDto;
-import com.salon.dto.shop.ReservationCheckDto;
-import com.salon.dto.shop.ReservationPreviewDto;
-import com.salon.dto.shop.ReservationRequestDto;
+import com.salon.dto.shop.*;
+import com.salon.entity.Member;
+import com.salon.entity.management.ShopDesigner;
+import com.salon.repository.MemberRepo;
+import com.salon.repository.management.ShopDesignerRepo;
+import com.salon.repository.management.master.ShopServiceRepo;
 import com.salon.service.shop.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -23,37 +28,62 @@ public class ReservationController {
 
 
     private final ReservationService reservationServcie;
+    private final ShopDesignerRepo shopDesignerRepo;
+    private final MemberRepo memberRepo;
 
 
 
     // 예약 작성 페이지 Get매핑
     @GetMapping("/write")
-    public String writeReservation(@RequestParam Long shopId, @RequestParam(required = false) Long shopDeisngerId,Model model) {
+    public String writeReservation(@RequestParam Long shopId, @RequestParam(required = false) Long shopDeisngerId, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate date,Principal principal ,Model model) {
 
-        // 디자이너 리스트 (선택이 안 된 경우에만 노출)
-        if (shopDeisngerId == null){
-            List<DesignerListDto> designerList = reservationServcie.getDesignerList(shopId);
-            model.addAttribute("designerList", designerList);
-            model.addAttribute("selectedDesignerId",null);
-        } else {
-            // 선택된 디자이너의 전문 시술 분야 가져오기
-            List<DesignerServiceCategoryDto> serviceCategories = reservationServcie.getDesignerServiceCategoeies(shopDeisngerId);
-            model.addAttribute("serviceCategories", serviceCategories);
+
+        // 로그인 사용자 정보 바인딩
+        if (principal != null) {
+            String loginId = principal.getName();
+            Member member = memberRepo.findByLoginId(loginId);
+            model.addAttribute("member",member);
         }
 
-        model.addAttribute("shopId",shopId);
-        model.addAttribute("selectedDesignerId", shopDeisngerId);
+        model.addAttribute("shopId", shopId);
+        model.addAttribute("selectedDesginerId", shopDeisngerId);
+
+        if (shopDeisngerId == null){
+            // 디자이너 선택이 안 된 상태일때 - 디자이너 리스트만 보여주기
+            List<DesignerListDto> designerList = reservationServcie.getDesignerList(shopDeisngerId);
+            model.addAttribute("designerList",designerList);
+
+        }else {
+            // 디자이너가 선택된 경우
+            ShopDesigner shopDesigner = shopDesignerRepo.findByIdAndIsActiveTrue(shopDeisngerId);
+            if (shopDeisngerId == null){
+                throw new IllegalArgumentException("존재하지 않거나 비활성화된 디자이너 입니다 ID : "+shopDeisngerId);
+            }
+            model.addAttribute("designer",shopDesigner);
+
+            List<DesignerServiceCategoryDto> serviceCategories  = reservationServcie.getDesignerServiceCategoeies(shopDeisngerId);
+            model.addAttribute("serviceCategories", serviceCategories);
+
+            AvailableTimeSlotDto timeSlotDto = reservationServcie.getAvailableTimeSlots(shopDeisngerId,date);
+            model.addAttribute("availableTimeSlot", timeSlotDto);
+        }
 
         return "shop/reservationSelect";
     }
 
     // 예약 작성 Post -> 예약 확인 페이지로
     @PostMapping("/comfirm")
-    public String confirmReservaiton(@ModelAttribute ReservationRequestDto requestDto, Model model){
+    public String confirmReservaiton(@ModelAttribute ReservationRequestDto requestDto, Principal principal, Model model){
+
+        // 예약자 정보 조회
+        String loginId = principal.getName();
+        Member member = memberRepo.findByLoginId(loginId);
+
 
         // 예약확인용 dto 구성
         ReservationCheckDto checkDto = reservationServcie.bulidReservationCheck(requestDto);
 
+        model.addAttribute("memeber",member); // 예약자 정보
         model.addAttribute("checkDto", checkDto); // 예약 확인 화면에 보여줄 dto
         model.addAttribute("requestDto", requestDto); // 이후 저장시 그대로 넘길 원본 dto
 
@@ -62,7 +92,13 @@ public class ReservationController {
 
     // 예약 확인 페이지 Get
     @GetMapping("/confirm")
-    public String confirmReservationPage(@ModelAttribute ReservationRequestDto requestDto, Model model){
+    public String confirmReservationPage(@ModelAttribute ReservationRequestDto requestDto,Principal principal ,Model model){
+
+        // 예약자 정보 조회
+        String loginId = principal.getName();
+        Member member = memberRepo.findByLoginId(loginId);
+        model.addAttribute("member", member);
+
 
         // 예약 정보 요약 (디자이너, 시술, 날짜 등)
         ReservationPreviewDto preview = reservationServcie.getReservationPreview(requestDto.getMemberId(),requestDto.getShopDesignerId(),
@@ -87,10 +123,16 @@ public class ReservationController {
 
     // 예약 확정 및 저장 (post)
     @PostMapping("complete")
-    public String completeReservation(@ModelAttribute ReservationRequestDto requestDto, RedirectAttributes redirect){
+    public String completeReservation(@ModelAttribute ReservationRequestDto requestDto, RedirectAttributes redirect,Principal principal,Model model){
+
+        // 예약자 정보 조회
+        String loginId = principal.getName();
+        Member member = memberRepo.findByLoginId(loginId);
+        model.addAttribute("memeber",member); // 예약자 정보
 
 
         reservationServcie.saveReservation(requestDto);
+
 
         redirect.addFlashAttribute("message", "예약이 성공적으로 완료 되었습니다");
         return "redirect:/shop/complete";
