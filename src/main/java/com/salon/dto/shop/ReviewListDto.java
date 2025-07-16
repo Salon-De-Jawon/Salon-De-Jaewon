09286162f26243b2f2836241729ee6ff59fe413b
Fi2a <fia_to@naver.com>
2025-07-16 15:40:39 +0900

    재원 0716 shopDetail 리뷰 완료

--- src/main/java/com/salon/dto/shop/ReviewListDto.java
+++ src/main/java/com/salon/dto/shop/ReviewListDto.java
@@ -1,5 +1,6 @@
 package com.salon.dto.shop;
 
+import com.salon.constant.ServiceCategory;
 import com.salon.dto.designer.ReviewReplyDto;
 import com.salon.entity.Member;
 import com.salon.entity.Review;
@@ -29,6 +30,8 @@ public class ReviewListDto {
     private int designerWorkingYears; // 시술한 디자이너 연차
     private String createdDateFormetted; // 리뷰 날짜
 
+    private String category; // 카테고리
+
     private List<ReviewImageDto> imageDtos = new ArrayList<>(); // 리뷰 이미지
     private ReviewReplyDto reply; // 디자이너 답글 dto
 
@@ -46,7 +49,8 @@ public class ReviewListDto {
         reviewListDto.setVisitCount(visitCount);
         reviewListDto.setDesignerName(shopDesigner.getDesigner().getMember().getName());
         reviewListDto.setDesignerWorkingYears(shopDesigner.getDesigner().getWorkingYears());
-
+        reviewListDto.setMemberName(review.getReservation().getMember().getName());
+        reviewListDto.setCategory(review.getReservation().getShopService().getCategory().getLabel());
 
         // 시술명
         Reservation res = review.getReservation();
