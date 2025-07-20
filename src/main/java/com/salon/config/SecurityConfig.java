007967976a4a114bdc4d9dc1a8bb519d3dec0368
dmsgml7476 <dmsgml7476@gmail.com>
2025-07-21 08:44:43 +0900

    미용사 신청

--- src/main/java/com/salon/config/SecurityConfig.java
+++ src/main/java/com/salon/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                         .requestMatchers("/master/**").hasRole("MAIN_DESIGNER") // 메인디자이너 페이지
                         .requestMatchers("/admin/**","admin/cs/**", "admin/anc/**").hasRole("ADMIN")
                         .requestMatchers("/auth/email/**").permitAll()
-                        .requestMatchers("/master/shop-edit/**", "/master/**").permitAll()
+                        .requestMatchers("/master/shop-edit/**", "/master/**", "/cs/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .userDetailsService(customUserDetailsService)
